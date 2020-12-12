d68d8644c8dc57b2d49f6b3eac1273ea117aba2c
AidanDennehy <44627373+Dracfo@users.noreply.github.com>
2020-12-12 15:52:22 -0500

    Added django-heroku

--- RolesScheduler/settings.py
+++ RolesScheduler/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,4 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+django_heroku.settings(locals())
